6c24ea3cddcfd5ddb0d2d653f4a49ba43f1fc821
alim509 <tul56193@temple.edu>
2024-01-24 22:54:59 -0500

    Initialize level

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import pygame, sys
 from settings import *
+from level import Level
 
 class Game:
     def __init__(self):
         # general setup
         pygame. init ()
         self.screen = pygame.display. set_mode( (WIDTH,HEIGHT))
-        self. clock = pygame. time.Clock()
+        pygame.display.set_caption('Adventure Thyme')
+        self.clock = pygame. time.Clock()
+        self.level = Level()
 
     def run(self):
         while True:
@@ -16,6 +19,7 @@ class Game:
                     sys. exit()
 
             self.screen. fill('black')
+            self.level.run()
             pygame.display.update()
             self.clock.tick (FPS)
 
